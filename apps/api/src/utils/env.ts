2515e90390792e715a881519217a9f6c8aee147d
this-is-tobi <thibault_colin@hotmail.fr>
2023-10-15 19:31:41 +0200

    fix: fix api swagger host variable

--- apps/api/src/utils/env.ts
+++ apps/api/src/utils/env.ts
@@ -1,4 +1,4 @@
 // api
-export const host: string = process.env.API_PORT ?? 'localhost'
+export const host: string = process.env.API_HOST ?? 'localhost'
 export const port: string = process.env.API_PORT ?? '8080'
 export const appVersion: string = process.env.APP_VERSION ?? 'dev'
