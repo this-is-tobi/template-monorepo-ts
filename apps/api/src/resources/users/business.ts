1c280b746105ab540687a8c5774cce76dfdbf898
this-is-tobi <thibault_colin@hotmail.fr>
2024-02-13 04:14:58 +0100

    feat: add bun types for api

--- apps/api/src/resources/users/business.ts
+++ apps/api/src/resources/users/business.ts
@@ -1,17 +1,17 @@
 import { randomUUID } from 'node:crypto'
 import type { FastifyRequest } from 'fastify'
-import { type User, UserSchema } from '@template-monorepo-ts/shared'
+import { type User } from '@template-monorepo-ts/shared'
 import { addReqLogs } from '@/utils/index.js'
 import { createUserQuery, deleteUserQuery, getUserByIdQuery, getUsersQuery, updateUserQuery } from './queries.js'
 
-export const createUserValidation = async (req: FastifyRequest, data: Omit<User, 'id'>) => {
-  const validation = UserSchema.omit({ id: true }).safeParse(data)
-  if (!validation.success) {
-    const message = 'failed to validate user schema'
-    addReqLogs({ req, message, error: validation.error })
-  }
-  return validation
-}
+// export const createUserValidation = async (req: FastifyRequest, data: Omit<User, 'id'>) => {
+//   const validation = UserSchema.omit({ id: true }).safeParse(data)
+//   if (!validation.success) {
+//     const message = 'failed to validate user schema'
+//     addReqLogs({ req, message, error: validation.error })
+//   }
+//   return validation
+// }
 
 export const createUser = async (req: FastifyRequest, data: Omit<User, 'id'>) => {
   const user = await createUserQuery({ id: randomUUID(), ...data })
