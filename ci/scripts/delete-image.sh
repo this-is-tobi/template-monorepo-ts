3a29032136a2756ddfa1151bb9817a2ffd27fb8f
this-is-tobi <this-is-tobi@proton.me>
2024-08-04 00:32:13 +0200

    refactor: improve ci system and permissions

--- ci/scripts/delete-image.sh
+++ ci/scripts/delete-image.sh
@@ -6,6 +6,9 @@ set -e
 red='\e[0;31m'
 no_color='\033[0m'
 
+# Default
+MODE="users"
+
 # Get versions
 DOCKER_VERSION="$(docker --version)"
 DOCKER_BUILDX_VERSION="$(docker buildx version)"
@@ -18,7 +21,9 @@ Following flags are available:
 
   -i    Image name used for api calls.
 
-  -o    Github organization used for api calls.
+  -m    Mode to use (`orgs` or `users`).
+
+  -o    Github owner (organization or user) used for api calls.
 
   -t    Image tage to delete.
 
@@ -29,14 +34,16 @@ print_help() {
 }
 
 # Parse options
-while getopts hg:i:o:t: flag; do
+while getopts hg:i:m:o:t: flag; do
   case "${flag}" in
     g)
       GITHUB_TOKEN=${OPTARG};;
     i)
       IMAGE_NAME=${OPTARG};;
+    m)
+      MODE=${OPTARG};;
     o)
-      ORG=${OPTARG};;
+      OWNER=${OPTARG};;
     t)
       TAG=${OPTARG};;
     h | *)
@@ -66,7 +73,7 @@ printf "\nScript settings:
   -> docker buildx version: ${DOCKER_BUILDX_VERSION}\n"
 
 
-if [ -z "$GITHUB_TOKEN" ] || [ -z "$ORG" ] || [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
+if [ -z "$GITHUB_TOKEN" ] || [ -z "$OWNER" ] || [ -z "$IMAGE_NAME" ] || [ -z "$TAG" ]; then
   echo "\nYMissing arguments ...\n"
   print_help
   exit 1
@@ -75,25 +82,25 @@ fi
 IMAGE_NAME_URL_ENCODED="$(jq -rn --arg x ${IMAGE_NAME} '$x | @uri')"
 IMAGES=$(curl -s \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions?per_page=100")
+  "https://api.github.com/${MODE}/${OWNER}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions?per_page=100")
 MAIN_IMAGE_ID=$(echo "$IMAGES" | jq -r --arg t "$TAG" '.[] | select(.metadata.container.tags[] | contains($t)) | .id')
 
 # Delete subsequent images
 while read -r SHA; do
-  IMAGE_ID=$(echo "$IMAGES" | jq -r --arg s "$SHA" '.[] | select(.name==$s) | .id')
+  IMAGE_ID=$(echo "$IMAGES" | jq -r --arg s "$SHA" '.[] | select(.name == $s) | .id')
 
-  printf "\n${red}[Delete ghcr image].${no_color} Deleting subsequent image '$ORG/$IMAGE_NAME@$SHA'\n"
+  printf "\n${red}[Delete ghcr image].${no_color} Deleting subsequent image '$OWNER/$IMAGE_NAME@$SHA'\n"
 
   curl -s \
     -X DELETE \
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${IMAGE_ID}"
-done <<< "$(docker buildx imagetools inspect ghcr.io/${ORG}/${IMAGE_NAME}:${TAG} --raw | jq -r '.manifests[] | .digest')"
+    "https://api.github.com/${MODE}/${OWNER}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${IMAGE_ID}"
+done <<< "$(docker buildx imagetools inspect ghcr.io/${OWNER}/${IMAGE_NAME}:${TAG} --raw | jq -r '.manifests[] | .digest')"
 
 # Delete main image
-printf "\n${red}[Delete ghcr image].${no_color} Deleting image '$ORG/$IMAGE_NAME:$TAG'\n"
+printf "\n${red}[Delete ghcr image].${no_color} Deleting image '$OWNER/$IMAGE_NAME:$TAG'\n"
 
 curl -s \
   -X DELETE \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "https://api.github.com/orgs/${ORG}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${MAIN_IMAGE_ID}"
+  "https://api.github.com/${MODE}/${OWNER}/packages/container/${IMAGE_NAME_URL_ENCODED}/versions/${MAIN_IMAGE_ID}"
