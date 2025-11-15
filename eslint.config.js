73078ae4fe1e7c9012ffb4c816605f5d3f0cac28
this-is-tobi <this-is-tobi@proton.me>
2025-11-15 20:51:42 +0100

    build: upgrade js dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import eslintConfigBase from '@template-monorepo-ts/eslint-config'
 export default eslintConfigBase.append({
   ignores: [
     'apps/**/src',
+    'apps/**/prisma',
     'packages/**/src',
   ],
 })
